bcde45f1b44f2e7c9b24c3727cc26d8e89afd392
juanpuleo66 <juanpuleo@gmail.com>
2019-10-01 22:58:48 -0400

    Course 18-2

--- playground/1-json.js
+++ playground/1-json.js
@@ -7,9 +7,25 @@ const fs = require('fs')
 
 // const bookJSON = JSON.stringify(book)
 
-// fs.writeFileSync('1-JSON.json', bookJSON)
+// fs.writeFileSync('1-json.json', bookJSON)
 
-const dataBuffer = fs.readFileSync('1-JSON.json')
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const parseData = JSON.parse(dataJSON) 
+// console.log('parseData: ',parseData)
+
+const dataBuffer = fs.readFileSync('1-json.json')
+console.log('dataBuffer: ',dataBuffer)
 const dataJSON = dataBuffer.toString()
-const parseData = JSON.parse(dataJSON) 
+console.log('dataJSON: ',dataJSON)
+let parseData = JSON.parse(dataJSON)
 console.log('parseData: ',parseData)
+const largo = parseData.length
+const objeto = {name:'Ana', planet:'SaturnoMercurio', age:55}
+parseData = [...parseData, objeto]
+console.log('parseData: ',parseData)
+
+const stringData = JSON.stringify(parseData)
+fs.writeFileSync('1-json.json',stringData)
+// fs.appendFileSync('1-json.json',stringData)
+// fs.appendFileSync('1-json.json','/n')
